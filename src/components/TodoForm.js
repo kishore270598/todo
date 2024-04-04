6f12c211c3848bd789bc12edd1e65b13b99fa3d6
kishore270598 <kk94323@gmail.com>
2024-04-04 11:46:41 +0530

    Todofrom and wrapper js  added

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const Todoform = () => {
   return (
-    <div>Todoform</div>
+    <form className='Todoform'>
+      <input type='Text' className='Todo-input' 
+      placeholder='What is the task today!' />
+      <button type='submit' className='tod-btn'/>
+    </form>
+
   )
 }
