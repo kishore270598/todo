925bb6b0ea043adbb33882a1ccf3862c0ee50f0b
kishore270598 <kk94323@gmail.com>
2024-04-06 21:15:49 +0530

    added Id ,task

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import { Todoform } from './TodoForm'
-
+import React ,{useState} from 'react'
+import { TodoForm } from './TodoForm'
+import { v4 as uuidv4 } from 'uuid';
+uuidv4();
 //export--< public>
 export const TodoWrapper = () => {
+  const [todos, setTodos] =useState([])
+  //sub function
+  const addTodo = todo => {
+    //clone
+    const temp=[...todos]
+    // id - unique id,task-- the user given task , completed- done or not,isEditing 
+    const newtodo={id: uuidv4(),
+      task: todo,
+      completed: false,
+      isEditing: false
+    }
+    temp.push(newtodo)
+    console.log(temp)
+    setTodos(temp)
+    console.log(todos)
+  }
   return (
     <div className='TodoWrapper'>
-      <Todoform />
+      <TodoForm addTodo={addTodo}/>
     </div>
   )
 }
